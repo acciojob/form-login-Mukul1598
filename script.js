6d04a6281a28ca2f03cc9eca46ec140190a119f1
Mukul Singh <135323609+Mukul1598@users.noreply.github.com>
2024-05-16 23:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let first_name = document.getElementById("fname").value
+	let last_name = document.getElementById("lname").value
+	let fullName = first_name + " " + last_name;
+	alert(fullName);
 }
